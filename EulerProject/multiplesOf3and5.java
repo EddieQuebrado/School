05321e46a7f2fcc05f29b6fd1fccd30f079f2a1d
unknown <eddiequebrado@u.boisestate.edu>
2020-09-02 16:44:30 -0600

    EulerProject repo

---
+++ EulerProject/multiplesOf3and5.java
@@ -0,0 +1,25 @@
+public class multiplesOf3and5 {
+	final static int LIMIT = 1000;
+
+	public static void main(String args[]) throws IOException {
+		long result = solution(LIMIT);
+		System.out.println("The sum of multiples under " + LIMIT + ": " + result);
+	}
+	
+	/**
+	 * Finds the sum of all multiples of 3 or 5 under a limit.
+	 * @param limit
+	 * @return sum of all multiples of 3 or 5
+	 */
+	public static long solution(int limit){
+		long sum = 0;
+		for(int i = 0; i < limit; ++i){
+			if(i%3==0){
+				sum += i;
+			} else if(i%5==0){
+				sum += i;
+			}
+		}
+		return sum;
+	}
+}
